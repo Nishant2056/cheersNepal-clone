215200cd123a14b559e7270d4642563d3385c61d
nishant bhurtel <bhurtelnishant@gmail.com>
2026-01-27 15:14:32 +0545

    changes in the category dropdown and logout button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,6 @@ import {
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { logOut, selectCurrentToken } from "../../redux/api/authSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { IoMdLogOut } from "react-icons/io";
 
 const Header = () => {
   const { data, error, isLoading } = useGetCategoriesQuery();
@@ -119,8 +118,8 @@ const Header = () => {
 
                 <ul className={`${css.categoryList} dropdown-menu`}>
                   {stores.map((store) => (
-                    <div key={store.id} className="mb-5">
-                      <h3 className="mb-4 border-bottom pb-2 text-center">
+                    <div key={store.id} className="mb-2">
+                      <h3 className="mb-2 pb-2 text-center">
                         {store.store_name}
                       </h3>
 
@@ -128,7 +127,7 @@ const Header = () => {
                         Object.values(store.category).map((cat) => (
                           <li
                             key={cat.id}
-                            className={` ${css.categoryListItem} mb-2 list-unstyled p-2`}
+                            className={` ${css.categoryListItem} list-unstyled p-2 border-bottom`}
                           >
                             <a
                               href="#"
@@ -162,7 +161,7 @@ const Header = () => {
             </div>
           </div>
 
-          <div className="col-2 d-flex align-items-center justify-content-end">
+          <div className="col-1 d-flex align-items-center d-flex justify-content-center">
             <div className="position-relative">
               <img
                 src={Buy}
@@ -178,14 +177,14 @@ const Header = () => {
             </div>
           </div>
           {token && (
-            <div className="col-1 text-end d-flex align-items-center justify-content-center">
+            <div className="col-2 text-end d-flex align-items-center justify-content-center">
               <button
                 type="button"
-                className="btn btn-success d-flex align-items-center justify-content-center"
+                className={`${css.logoutBtn} btn btn-success d-flex align-items-center justify-content-center`}
                 onClick={handleLogout}
               >
                 {" "}
-                <IoMdLogOut />
+                Logout
               </button>
             </div>
           )}
